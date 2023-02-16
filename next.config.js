ff9aad4f1d9c522d5768faea3835b956a7d6526f
siavashbis <siavashbis@gmail.com>
2023-02-16 16:17:20 +0330

    c

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
+images: {
+  domains: [
+    'https://my-coffee-shop-next-4hqj50itt-siavashbis-gmailcom.vercel.app/',
+  ]
+}
 module.exports = nextConfig
